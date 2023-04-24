fd7236ba6cea828e131ca3a8d0e6b63958b4ef8d
mkerkstra <mattkerkstra@gmail.com>
2023-04-24 17:57:12 +0000

    read image in as a stream

--- apps/api-flask/app.py
+++ apps/api-flask/app.py
@@ -2,17 +2,22 @@ from flask import Flask, jsonify, request
 from fastai.vision.all import *
 
 app = Flask(__name__)
+model = load_learner('models/hotdog_nothotdog.pkl')
 
 @app.route('/classify', methods=['POST'])
 def classify_image():
     print(request)
+    if 'file' not in request.files:
+        return 'No file provided', 400
     # Load the trained model and transform
-    model = load_learner('models/hotdog_nothotdog.pkl')
     # Get the image data from the request object
-    img_file = request.files['image0']
-    img_bytes = img_file.read()
-    img = PILImage.create(img_bytes)
 
+    # Format the image data
+    stream = request.files['file'].stream
+    image = PILImage.open(stream)
+    image = image.resize((192, 192), 'squish')
+    img_bytes = image.read()
+    img = PILImage.create(img_bytes)
     # Make a prediction and return the results
     pred, label, probs = model.predict(img)
     response = {'prediction': str(label), 'probabilities': probs.tolist()}
